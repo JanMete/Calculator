6c4cc52646617b50c6477d902e7cc7334d81191f
Jan Metelański <metelanskij@gmail.com>
2023-12-12 06:25:48 +0100

    commiting the project

---
+++ src/Components/DigitButton.jsx
@@ -0,0 +1,11 @@
+import { ACTIONS } from '../Utils/ACTIONS';
+
+export default function OperationButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
